3282c92a3aa136e448799bd29f986e91764e5d06
shikhars22 <shikhars22@gmail.com>
2021-06-11 16:57:10 +0530

    added -y tag and added local logging into log file also

--- automation.sh
+++ automation.sh
@@ -6,47 +6,58 @@
 #chmod 777 automation.sh
 #####################################################
 ######To check if Apache2 is installed or not ###########
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+myname="shikhar"
+s3_bucket="upgrad-shikhar"
+
+echo "-------------automation.sh--running on $timestamp------------" > $myname-automation-script-logs-$timestamp.log
+
+
 isApacheInstalled=$(( `dpkg --get-selections | grep apache | wc -l` ))
-#echo $isApacheInstalled
+echo "isApacheInstalled = $isApacheInstalled" >$myname-automation-script-logs-$timestamp.log
+
 
 if [ $isApacheInstalled == 0 ] 
 then
-    echo "Apache not installed"
+    echo "Apache not installed" >> $myname-automation-script-logs-$timestamp.log
     sudo apt update -y
-    sudo apt update
-    sudo apt install apache2
+    sudo apt install -y apache2
+    echo "Apache is now installed" >> $myname-automation-script-logs-$timestamp.log
 else
-    echo "Apache already installed"
+    echo "Apache already installed" >> $myname-automation-script-logs-$timestamp.log
 fi
 
 isApacheRunning=$(( `sudo systemctl status apache2 | grep active | wc -l` ))
+echo "isApacheRunning = $isApacheRunning" >> $myname-automation-script-logs-$timestamp.log
+
 if [ $isApacheRunning == 1 ]
 then
-    echo "Apache server is running"
+    echo "Apache server is running" >> $myname-automation-script-logs-$timestamp.log
 else
-    echo "Apache is server is not running"
+    echo "Apache is server is not running" >> $myname-automation-script-logs-$timestamp.log
     sudo systemctl start apache2
     sudo systemctl enable apache2
+    echo "Apache is server is now running and enabled" >> $myname-automation-script-logs-$timestamp.log
 fi
 
 ######To check if AWS CLI is installed or not ###########
 isAwsCliInstalled=$(( `dpkg --get-selections | grep awscli | wc -l` ))
-#echo $isAwsCliInstalled
+echo "isAwsCliInstalled = $isAwsCliInstalled" >> $myname-automation-script-logs-$timestamp.log
 
 if [ $isAwsCliInstalled == 0 ]
 then
-    echo "AWS CLI not installed"
-    sudo apt update
-    sudo apt install awscli
+    echo "AWS CLI not installed" >> $myname-automation-script-logs-$timestamp.log
+    sudo apt update -y
+    sudo apt install -y awscli
+    echo "AWS CLI now installed" >> $myname-automation-script-logs-$timestamp.log
 else
-    echo "AWS CLI is already installed"
+    echo "AWS CLI is already installed" >> $myname-automation-script-logs-$timestamp.log
 fi
 
 ######To create tar file of apache log files ###########
 cd /var/log/apache2/
-timestamp=$(date '+%d%m%Y-%H%M%S')
-myname="shikhar"
-s3_bucket="upgrad-shikhar"
+
 find . -iname '*.log' -print0 | xargs -0 tar zcf $myname-httpd-logs-$timestamp.tar
 echo "Tar file is created in /tmp/ and name of tar file is $myname-httpd-logs-$timestamp.tar"
 echo "Contents of $myname-httpd-logs-$timestamp.tar are as follows:"
